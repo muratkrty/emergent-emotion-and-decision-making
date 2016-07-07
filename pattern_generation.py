93694991469c561a32d7c55279fa18afd004757c
muratkrty <muratkrty@gmail.com>
2016-07-07 14:09:35 +0200

    Updated resizing for display purpose

--- pattern_generation.py
+++ pattern_generation.py
@@ -13,17 +13,28 @@ def main():
     resized = 'patterns/resized'
     binarized = 'patterns/binarized'
 
-    folders = {0.05:'patterns/noise/5p', 0.15:'patterns/noise/15p',
-               0.25:'patterns/noise/25p', 0.4:'patterns/noise/40p',
-               0.5:'patterns/noise/50p', 0.75:'patterns/noise/75p', 
-               1 :'patterns/noise/100p'}
+    # d folders are used for displa purpose
+    dfolders = {0 :'dpatterns/noise/0p', 0.05:'dpatterns/noise/5p', 
+               0.15:'dpatterns/noise/15p', 0.25:'dpatterns/noise/25p', 
+               0.4:'dpatterns/noise/40p', 0.5:'dpatterns/noise/50p', 
+               0.75:'dpatterns/noise/75p', 1 :'dpatterns/noise/100p'}
 
-    resize_wh = (20, 20)
+    folders = {0 :'patterns/noise/0p', 0.05:'patterns/noise/5p', 
+               0.15:'patterns/noise/15p', 0.25:'patterns/noise/25p', 
+               0.4:'patterns/noise/40p', 0.5:'patterns/noise/50p', 
+               0.75:'patterns/noise/75p', 1 :'patterns/noise/100p'}
+
+    resize_wh = (20, 20) 
+    # d folders image process
+    dresize_wh = (1000, 1000)
     imp.resize_patterns(training, resized, resize_wh)
     imp.binarize_patterns(resized, binarized)
 
     for percent in folders:
-        imp.create_noisy_patterns(binarized, folders[ percent],  percent)
+        imp.create_noisy_patterns(binarized, folders[percent],  percent)
+
+    for key in folders:
+        imp.resize_patterns(folders[key], dfolders[key], dresize_wh) 
 
 if __name__ == '__main__':
     main()
